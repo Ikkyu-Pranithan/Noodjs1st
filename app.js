3bfe6671e422d0a4cfdd19943677a9fa1e305c5b
Ikkyu CRS <pranithan.ikkyu@gmail.com>
2023-08-21 21:33:19 +0700

    Add Template

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require('express');
 const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
+const path = require('path');
+
 const app = express();
 const port = 3000;
 
 app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname,"/public/")));
 
 app.get("/", (req,res) =>{
 
@@ -15,4 +18,4 @@ app.get("/", (req,res) =>{
 
 app.listen(port, ()=>{
     debug("Listening on port" + chalk.green(" : "+port));
-})
+}) 
